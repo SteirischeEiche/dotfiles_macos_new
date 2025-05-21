433c9d8ff93a638870f1c0ae6f25ea35696e2c40
SteirischeEiche <210909647+SteirischeEiche@users.noreply.github.com>
2025-05-21 11:33:10 +0200

    Change some Finder settings

---
+++ setup_macos.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Starting macOS Setup >>>\n"
+
+# Use list view
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+# Show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+# Show filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+# Search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+# Keep folders on top when sorting by name
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+killall Finder
